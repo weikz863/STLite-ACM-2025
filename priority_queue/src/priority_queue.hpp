f2a39d58ba871c6f55d708b86351da2a26ad6ca2
weikz <weikz@sjtu.edu.cn>
2025-03-16 20:45:28 +0800

    more memory management

--- priority_queue/src/priority_queue.hpp
+++ priority_queue/src/priority_queue.hpp
@@ -8,17 +8,17 @@
 namespace sjtu {
 
 template<class T>
-class shared_ptr {
+class SharedPtr {
  private:
   T *get;
   size_t *cnt;
  public:
-  shared_ptr() : get(nullptr), cnt(nullptr) {}
-  shared_ptr(T *ptr) : get(ptr), cnt(new size_t{1}) {}
-  shared_ptr(const shared_ptr &other) : get(other.get), cnt(other.cnt) {
+  SharedPtr() : get(nullptr), cnt(nullptr) {}
+  SharedPtr(T *ptr) : get(ptr), cnt(new size_t{1}) {}
+  SharedPtr(const SharedPtr &other) : get(other.get), cnt(other.cnt) {
     if (cnt) ++*cnt;
   }
-  shared_ptr &operator=(const shared_ptr &other) {
+  SharedPtr &operator=(const SharedPtr &other) {
     if (&other == this) return *this;
     this->reset();
     get = other.get;
@@ -27,7 +27,7 @@ class shared_ptr {
     return *this;
   }
   void reset(T *ptr) {
-    *this = shared_ptr(ptr);
+    *this = SharedPtr(ptr);
   }
   void reset() {
     if (cnt && --*cnt == 0) {
@@ -37,9 +37,30 @@ class shared_ptr {
     cnt = nullptr;
     get = nullptr;
   }
-  const T &operator*() const { return *get; }
-  T &operator*() { return *get; }
-  ~shared_ptr() { reset(); }
+	SharedPtr deep_copy() {
+		if (get != nullptr) return SharedPtr(new T(*get));
+		else return {};
+	}
+  const T& operator*() const { return *get; }
+  T& operator*() { return *get; }
+  const T* operator->() const { return get; }
+  T* operator->() { return get; }
+  ~SharedPtr() { reset(); }
+};
+
+template<class T>
+class UniqueArray {
+ private:
+  T *arr;
+ public:
+  UniqueArray(size_t size) : arr(new T[size]) {}
+  ~UniqueArray() { delete[] arr; }
+  T& operator[](size_t x) {
+    return arr[x];
+  }
+  const T& operator[](size_t x) const {
+    return arr[x];
+  }
 };
 
 /**
@@ -50,27 +71,30 @@ class shared_ptr {
 template<typename T, class Compare = std::less<T>>
 class priority_queue { // pairing heap
  private:
-  class node {
-    shared_ptr<const T> value;
-    shared_ptr<node> first_child;
-    shared_ptr<node> sibling;
-    node(const T & val, shared_ptr<node> fc = shared_ptr<node>(), shared_ptr<node> sib = shared_ptr<node>()) : 
+  class Node {
+    SharedPtr<const T> value;
+    SharedPtr<Node> first_child;
+    SharedPtr<Node> sibling;
+    Node(const T & val, SharedPtr<Node> fc = SharedPtr<Node>(), SharedPtr<Node> sib = SharedPtr<Node>()) : 
         value(new T(val)), first_child(fc), sibling(sib) {}
-    node(const node &other) : 
-		    value(other.value), first_child(other.first_child), sibling(other.sibling) {}
+    Node(const Node &other) : value(other.value.deep_copy()),
+		    first_child(other.first_child.deep_copy()), sibling(other.sibling.deep_copy()) {}
   };
-  shared_ptr<node> root;
+  SharedPtr<Node> root;
+	size_t size_;
+  std::function<bool(const T&, const T&)> cmp;
  public:
   /**
    * @brief default constructor
    */
-  priority_queue() : root{} {}
+  priority_queue() : root{}, size_{0}, cmp(Compare()) {}
 
   /**
    * @brief copy constructor
    * @param other the priority_queue to be copied
    */
-  priority_queue(const priority_queue &other) : root(new node(*other.root)) {}
+  priority_queue(const priority_queue &other) : 
+      root(other.root.deep_copy()), size_(other.size_), cmp(other.cmp) {}
 
   /**
    * @brief deconstructor
@@ -82,20 +106,31 @@ class priority_queue { // pairing heap
    * @param other the priority_queue to be assigned from
    * @return a reference to this priority_queue after assignment
    */
-  priority_queue &operator=(const priority_queue &other) {}
+  priority_queue &operator=(const priority_queue &other) {
+		root = other.root.deep_copy();
+		size_ = other.size_;
+    cmp = other.cmp;
+	}
 
   /**
    * @brief get the top element of the priority queue.
    * @return a reference of the top element.
    * @throws container_is_empty if empty() returns true
    */
-  const T & top() const {}
+  const T & top() const {
+		return *root->value;
+	}
 
   /**
    * @brief push new element to the priority queue.
    * @param e the element to be pushed
    */
-  void push(const T &e) {}
+  void push(const T &e) {
+    SharedPtr<Node> new_heap = new Node(e);
+    if (cmp(e, top())) {
+
+    }
+  }
 
   /**
    * @brief delete the top element from the priority queue.
@@ -107,13 +142,13 @@ class priority_queue { // pairing heap
    * @brief return the number of elements in the priority queue.
    * @return the number of elements.
    */
-  size_t size() const {}
+  size_t size() const { return size_; }
 
   /**
    * @brief check if the container is empty.
    * @return true if it is empty, false otherwise.
    */
-  bool empty() const {}
+  bool empty() const { return size_ == 0; }
 
   /**
    * @brief merge another priority_queue into this one.
