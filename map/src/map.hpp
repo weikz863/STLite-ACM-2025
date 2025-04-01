d8be94dd66bbee1a4b3521c35690899763a73f28
weikz <weikz@sjtu.edu.cn>
2025-04-01 20:44:01 +0800

    add basic iterator functions

--- map/src/map.hpp
+++ map/src/map.hpp
@@ -12,28 +12,42 @@
 
 namespace sjtu {
 
+class my_runtime_error : public sjtu::exception {
+ public:
+  my_runtime_error(const char *s) {
+    detail = std::string(s);
+  }
+};
+
 template<
     class Key,
     class T,
     class Compare = std::less <Key>
 > class map {
+ public:
+  /**
+   * the internal type of data.
+   * it should have a default constructor, a copy constructor.
+   * You can use sjtu::map as value_type by typedef.
+   */
+  typedef pair<const Key, T> value_type;
  private:
   struct Node {
     Node *parent, *left, *right;
     bool is_black;
-    Key *key;
-    T *value;
+    value_type *info;
     Node(Node *pa, Node *l = nullptr, Node *r = nullptr, bool isblack = false) :
-        parent(pa), left(l), right(r), is_black(isblack), key(nullptr), value(nullptr) {}
+        parent(pa), left(l), right(r), is_black(isblack), info(nullptr) {}
     Node() = delete;
     Node(const Node &other) = delete;
     ~Node() {
-      delete key;
-      delete value;
+      delete info;
+      delete left;
+      delete right;
     }
     Node& operator = (const Node &other) = delete;
     void rotate_right() {
-      if (!left) throw sjtu::runtime_error("right rotation failed");
+      if (!left) throw sjtu::my_runtime_error("right rotation failed");
       Node *tmp_left = left;
       set_left(this, tmp_left->right);
       set_right(tmp_left, this);
@@ -43,12 +57,12 @@ template<
         } else if (parent->right == *this) {
           parent->set_right(tmp_left);
         } else {
-          throw sjtu::runtime_error("father-son relationship broken");
+          throw sjtu::my_runtime_error("father-son relationship broken");
         }
       }
     }
     void rotate_left() {
-      if (!right) throw sjtu::runtime_error("left rotation failed");
+      if (!right) throw sjtu::my_runtime_error("left rotation failed");
       Node *tmp_right = right;
       set_right(this, tmp_right->left);
       set_left(tmp_right, this);
@@ -58,26 +72,37 @@ template<
         } else if (parent->right == *this) {
           parent->set_right(tmp_right);
         } else {
-          throw sjtu::runtime_error("father-son relationship broken");
+          throw sjtu::my_runtime_error("father-son relationship broken");
         }
       }
     }
   };
-  inline bool is_black(const Node *ptr) {
+  static inline bool is_black(const Node *ptr) {
     return !ptr || ptr->is_black;
   }
-  inline void set_left(Node *parent, Node *son) {
-    if (!parent) throw sjtu::runtime_error("set_left failed");
+  static inline void set_left(Node *parent, Node *son) {
+    if (!parent) throw sjtu::my_runtime_error("set_left failed");
     parent->left = son;
     if (son) son->parent = parent;
   }
-  inline void set_right(Node *parent, Node *son) {
-    if (!parent) throw sjtu::runtime_error("set_right failed");
+  static inline void set_right(Node *parent, Node *son) {
+    if (!parent) throw sjtu::my_runtime_error("set_right failed");
     parent->right = son;
     if (son) son->parent = parent;
   }
-  Node *next(Node *ptr) {
-    if (!ptr) throw sjtu::runtime_error("empty ptr for next");
+  static Node *deep_copy(Node *ptr, Node *pa) {
+    if (!pa) throw sjtu::my_runtime_error("deep copy with no parent");
+    if (!ptr) return nullptr;
+    Node *ret = new Node(pa);
+    ret->left = deep_copy(ptr->left, ret);
+    ret->right = deep_copy(ptr->right, ret);
+    ret->is_black = ptr->is_black;
+    if (!ptr->info) throw sjtu::my_runtime_error("deep copying virtual node");
+    ret->info = new value_type(*ptr->info);
+    return ptr;
+  }
+  static Node *next(Node *ptr) {
+    if (!ptr) throw sjtu::my_runtime_error("empty ptr for next");
     if (ptr->right) {
       ptr = ptr->right;
       while (ptr->left) {
@@ -91,8 +116,8 @@ template<
     }
     return ptr;
   }
-  Node *prev(Node *ptr) {
-    if (!ptr) throw sjtu::runtime_error("empty ptr for prev");
+  static Node *prev(Node *ptr) {
+    if (!ptr) throw sjtu::my_runtime_error("empty ptr for prev");
     if (ptr->left) {
       ptr = ptr->left;
       while (ptr->right) {
@@ -105,16 +130,16 @@ template<
       ptr = ptr->parent;
     }
   }
-  Node *lower_bound(Node *ptr, const Key *key) {
-    if (!ptr || !key) throw sjtu::runtime_error("empty ptr for lower_bound");
+  Node *try_find(Node *ptr, const Key &key) {
+    if (!ptr) throw sjtu::my_runtime_error("empty ptr for try_find");
     while (true) {
-      if (cmp(ptr->key, key)) {
+      if (cmp(ptr->info.first, key)) {
         if (ptr->right) {
           ptr = ptr->right;
         } else {
           return ptr->right = new Node(ptr);
         }
-      } else if (cmp(key, ptr->key)) {
+      } else if (cmp(key, ptr->info.first)) {
         if (ptr->left) {
           ptr = ptr->left;
         } else {
@@ -125,16 +150,10 @@ template<
       }
     }
   }
-  Node *root;
+  Node *root, *begin_;
   size_t size_;
   Compare cmp;
  public:
-  /**
-   * the internal type of data.
-   * it should have a default constructor, a copy constructor.
-   * You can use sjtu::map as value_type by typedef.
-   */
-  typedef pair<const Key, T> value_type;
   /**
    * see BidirectionalIterator at CppReference for help.
    *
@@ -144,101 +163,131 @@ template<
    */
   class const_iterator;
   class iterator {
+    friend class const_iterator;
+    friend class map;
    private:
-    /**
-     * TODO add data members
-     *   just add whatever you want.
-     */
+    Node *ptr;
+    iterator(Node *tmpptr) : ptr(tmpptr) {}
    public:
-    iterator() {
-      // TODO
+    iterator() : ptr(nullptr) {}
+    iterator(const iterator &other) : ptr(other.ptr) {}
+
+    iterator operator++(int) {
+      if (ptr->parent == ptr) throw sjtu::invalid_iterator();
+      Node *ret = ptr;
+      ptr = next(ptr);
+      return ret;
+    }
+    iterator &operator++() {
+      if (ptr->parent == ptr) throw sjtu::invalid_iterator();
+      ptr = next(ptr);
+      return *this;
+    }
+    iterator operator--(int) {
+      Node *ret = ptr;
+      ptr = prev(ptr);
+      if (ptr->parent == ptr) {
+        ptr = ret;
+        throw sjtu::invalid_iterator();
+      }
+      return ret;
+    }
+    iterator &operator--() {
+      Node *tmp = ptr;
+      ptr = prev(ptr);
+      if (ptr->parent == ptr) {
+        ptr = tmp;
+        throw sjtu::invalid_iterator();
+      }
+      return *this;
     }
 
-    iterator(const iterator &other) {
-      // TODO
+    value_type &operator*() const {
+      return *ptr->info;
     }
-
-    /**
-     * TODO iter++
-     */
-    iterator operator++(int) {}
-
-    /**
-     * TODO ++iter
-     */
-    iterator &operator++() {}
-
-    /**
-     * TODO iter--
-     */
-    iterator operator--(int) {}
-
-    /**
-     * TODO --iter
-     */
-    iterator &operator--() {}
-
-    /**
-     * a operator to check whether two iterators are same (pointing to the same memory).
-     */
-    value_type &operator*() const {}
-
-    bool operator==(const iterator &rhs) const {}
-
-    bool operator==(const const_iterator &rhs) const {}
-
-    /**
-     * some other operator for iterator.
-     */
-    bool operator!=(const iterator &rhs) const {}
-
-    bool operator!=(const const_iterator &rhs) const {}
-
-    /**
-     * for the support of it->first.
-     * See <http://kelvinh.github.io/blog/2013/11/20/overloading-of-member-access-operator-dash-greater-than-symbol-in-cpp/> for help.
-     */
-    value_type *operator->() const
-    noexcept {}
+    value_type *operator->() const noexcept {
+      return ptr->info;
+    }
+    bool operator==(const iterator &rhs) const { return ptr == rhs.ptr; }
+    bool operator==(const const_iterator &rhs) const { return ptr == rhs.ptr; }
+    bool operator!=(const iterator &rhs) const { return ptr != rhs.ptr; }
+    bool operator!=(const const_iterator &rhs) const { return ptr != rhs.ptr; }
   };
   class const_iterator {
-    // it should has similar member method as iterator.
-    //  and it should be able to construct from an iterator.
+    friend class iterator;
+    friend class map;
    private:
-    // data members.
+    Node *ptr;
+    const_iterator(Node *tmpptr) : ptr(tmpptr) {}
    public:
-    const_iterator() {
-      // TODO
+    const_iterator() : ptr(nullptr) {}
+    const_iterator(const const_iterator &other) : ptr(other.ptr) {}
+    const_iterator(const iterator &other) : ptr(other.ptr) {}
+    
+    const_iterator operator++(int) {
+      if (ptr->parent == ptr) throw sjtu::invalid_iterator();
+      Node *ret = ptr;
+      ptr = next(ptr);
+      return ret;
+    }
+    const_iterator &operator++() {
+      if (ptr->parent == ptr) throw sjtu::invalid_iterator();
+      ptr = next(ptr);
+      return *this;
+    }
+    const_iterator operator--(int) {
+      Node *ret = ptr;
+      ptr = prev(ptr);
+      if (ptr->parent == ptr) {
+        ptr = ret;
+        throw sjtu::invalid_iterator();
+      }
+      return ret;
+    }
+    const_iterator &operator--() {
+      Node *tmp = ptr;
+      ptr = prev(ptr);
+      if (ptr->parent == ptr) {
+        ptr = tmp;
+        throw sjtu::invalid_iterator();
+      }
+      return *this;
     }
 
-    const_iterator(const const_iterator &other) {
-      // TODO
+    const value_type &operator*() const {
+      return *ptr->info;
     }
-
-    const_iterator(const iterator &other) {
-      // TODO
+    const value_type *operator->() const noexcept {
+      return ptr->info;
     }
-    // And other methods in iterator.
-    // And other methods in iterator.
-    // And other methods in iterator.
+    bool operator==(const iterator &rhs) const { return ptr == rhs.ptr; }
+    bool operator==(const const_iterator &rhs) const { return ptr == rhs.ptr; }
+    bool operator!=(const iterator &rhs) const { return ptr != rhs.ptr; }
+    bool operator!=(const const_iterator &rhs) const { return ptr != rhs.ptr; }
   };
 
-  /**
-   * TODO two constructors
-   */
-  map() {}
+  map() : size_(0), cmp{} {
+    root = static_cast<Node*>(::operator new(sizeof(Node)));
+    ::new(root) Node(root, nullptr, root);
+  }
 
-  map(const map &other) {}
+  map(const map &other) : size_(other.size_), cmp{} {
+    root = static_cast<Node*>(::operator new(sizeof(Node)));
+    ::new(root) Node(root, deep_copy(other.root->left, root), root);
+  }
 
-  /**
-   * TODO assignment operator
-   */
-  map &operator=(const map &other) {}
+  map &operator=(const map &other) {
+    size_ = other.size_;
+    delete root->left;
+    root->left = deep_copy(other.root->left, root);
+    return *this;
+  }
 
-  /**
-   * TODO Destructors
-   */
-  ~map() {}
+  ~map() {
+    root->right = nullptr;
+    root->~Node();
+    ::operator delete(root);
+  }
 
   /**
    * TODO
@@ -264,36 +313,19 @@ template<
    */
   const T &operator[](const Key &key) const {}
 
-  /**
-   * return a iterator to the beginning
-   */
-  iterator begin() {}
+  iterator begin() { return next(root); }
+  const_iterator cbegin() const { return next(root); }
+  iterator end() { return root; }
+  const_iterator cend() const { return root; }
 
-  const_iterator cbegin() const {}
-
-  /**
-   * return a iterator to the end
-   * in fact, it returns past-the-end.
-   */
-  iterator end() {}
-
-  const_iterator cend() const {}
-
-  /**
-   * checks whether the container is empty
-   * return true if empty, otherwise false.
-   */
-  bool empty() const {}
-
-  /**
-   * returns the number of elements.
-   */
-  size_t size() const {}
-
-  /**
-   * clears the contents
-   */
-  void clear() {}
+  bool empty() const { return size_ == 0; }
+  size_t size() const { return size_; }
+  void clear() {
+    delete root->left;
+    root->left = nullptr;
+    size_ = 0;
+    begin_ = nullptr;
+  }
 
   /**
    * insert an element.
