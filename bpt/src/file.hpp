d495f90a0237d61cdfaee8e7cb5d13d9349c3b53
weikz <weikz@sjtu.edu.cn>
2025-04-28 16:53:58 +0800

    add file-based storage

--- bpt/src/file.hpp
+++ bpt/src/file.hpp
@@ -9,31 +9,34 @@ using std::ifstream, std::ofstream, std::fstream;
 using sjtu::vector;
 
 class BasicStorage {
+ protected:
+  bool initialized_;
  public:
   BasicStorage() = delete;
-  BasicStorage(const char *name) {}
+  BasicStorage(const char *name) : initialized_(false) {}
   BasicStorage(const BasicStorage &) = delete;
   BasicStorage(BasicStorage &&) = delete;
   BasicStorage& operator = (const BasicStorage &) = delete;
   BasicStorage& operator = (BasicStorage &&) = delete;
   virtual void write(int place, const char *value, size_t bytes) = 0;
   virtual void read(int place, char *value, size_t bytes) = 0;
-  template<typename T>
+  template<typename T> requires std::is_trivially_copyable<T>::value
   void write_at(int place, const T& value) {
     write(place, reinterpret_cast<const char*>(&value), sizeof(T));
   }
-  template<typename T>
+  template<typename T> requires std::is_trivially_copyable<T>::value
   void read_at(int place, T& value) {
     read(place, reinterpret_cast<char*>(&value), sizeof(T));
   }
-  template<typename T, std::size_t N>
+  template<typename T, std::size_t N> requires std::is_trivially_copyable<T>::value
   void write_at(int place, const T (&value)[N]) {
     write(place, reinterpret_cast<const char*>(value), sizeof(value));
   }
-  template<typename T, std::size_t N>
+  template<typename T, std::size_t N> requires std::is_trivially_copyable<T>::value
   void read_at(int place, T (&value)[N]) {
     read(place, reinterpret_cast<char*>(value), sizeof(value));
   }
+  bool& initialized() { return initialized_; }
 };
 
 class VectorStorage : public BasicStorage {
@@ -53,6 +56,28 @@ class VectorStorage : public BasicStorage {
     for (int i = 0; i < bytes; i++) {
       value[i] = data[place + i];
     }
+  }
+};
+
+class FileStorage : public BasicStorage {
+ private:
+  fstream file;
+ public:
+  FileStorage(const char *name) : BasicStorage(name) {
+    file.open(name, std::ios_base::in | std::ios_base::out | std::ios_base::binary);
+    if (file.is_open()) {
+      initialized_ = true;
+    } else {
+      file.open(name, std::ios_base::in | std::ios_base::out | std::ios_base::trunc | std::ios::binary);
+    }
+  }
+  void write(int place, const char *value, size_t bytes) override {
+    file.seekp(place);
+    file.write(value, bytes);
+  }
+  void read(int place, char *value, size_t bytes) override {
+    file.seekp(place);
+    file.read(value, bytes);
   }
 };
 
